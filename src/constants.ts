13ae6be2646a2e75d0db384f18e1d002adde64ea
tahfeezmizan <mizanahmed5316@gmail.com>
2025-11-03 01:00:24 +0600

    Update API base URL to production endpoint; comment out local development URLs

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 // const baseUrl = "http://localhost:3000/api";
-// const baseUrl = "https://library.mdranju.xyz/api";
-const baseUrl = "http://localhost:5000/api";
+const baseUrl = "https://libra-lite-backend.vercel.app/api";
+// const baseUrl = "http://localhost:5000/api";
 
 export const BASE_URL = baseUrl;
